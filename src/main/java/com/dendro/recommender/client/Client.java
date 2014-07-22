b8850d96fbc146d099f44965aeeaf89d3993498c
João Rocha <joaorocha@gnosis.local>
2014-07-22 15:39:18 +0100

    Initial commit

---
+++ src/main/java/com/dendro/recommender/client/Client.java
@@ -0,0 +1,8 @@
+package com.dendro.recommender.client;/**
+ * Created by joaorocha on 22/07/14.
+ */
+public class Client {
+  public static void main(String[] argv) {
+      System.out.println("Cliente iniciado");
+  }
+}
